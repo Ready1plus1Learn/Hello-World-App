dfd6ef912864d78e2647a471b55160be6398b644
David Christian <97576285+Ready1plus1Learn@users.noreply.github.com>
2024-04-15 15:57:37 +0800

    Add files via upload

---
+++ helloWorldApp.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class helloWorldApp{
+    public static void main(String[] args) {
+
+
+        System.out.println("Kamusta Mundo");
+
+    }
+}
